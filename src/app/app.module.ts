a1ab70519de063927f87741247422b0d6ad5f6fc
nitsu <nitsuzxz@gmail.com>
2023-11-01 11:19:57 +0800

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,20 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { StarRatingComponent } from './star-rating/star-rating.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { RouterModule } from '@angular/router';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductsComponent,
     StarRatingComponent,
-   
+    ProductDetailComponent  
 
   ],
   imports: [
@@ -23,6 +28,13 @@ import { StarRatingComponent } from './star-rating/star-rating.component';
     FormsModule,
     CommonModule,
     HttpClientModule,
+    RouterModule.forRoot([
+      {path:"products", component:ProductsComponent},
+      {path:"products/id", component:ProductDetailComponent},
+      {path:"welcome", component:WelcomeComponent},
+      {path:"", redirectTo:"welcome", pathMatch:"full"},
+      {path:"**", redirectTo:"welcome", pathMatch:"full"},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
