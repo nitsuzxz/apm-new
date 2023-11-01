1c3dff3ea84bbed95dcbffdd2dd443d2e584ec95
nitsu <nitsuzxz@gmail.com>
2023-11-01 14:18:11 +0800

    fetch product data

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IProduct } from 'src/app/data/models/products';
+import { ProductService } from 'src/app/data/services/product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -7,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
 
   pageTitle:string="Product Details";
+  product:IProduct | undefined;
+  errorMessage:string='';
+  sub!: Subscription;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) { }
 
   ngOnInit(): void {
+
+    const id= Number(this.route.snapshot.paramMap.get("id"));
+    this.pageTitle+=`: ${id}`;
+    this.getProduct(id);
   }
 
+  onBack():void{
+    this.sub.unsubscribe();
+    this.router.navigate(["/products"]);
+  }
+
+  getProduct(id:number):void{
+
+    this.sub=this.productService.getProduct(id).subscribe({
+      next: product => {
+        this.product= product;
+        
+      },
+      error: err => this.errorMessage =err
+    })
+  }
 }
